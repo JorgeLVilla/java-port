d8342d531bae092f733469049d990f68c55dd68a
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-10-17 15:00:56 -0700

    update depend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import test.js from
 
 function App() {
   return (
---
+++ src/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const calls = [
+    {name: "jamie", numOfCall: 7},
+    {name: "Anna", numOfCall: 4},
+    {name: "Tony", numOfCall: 12},
+    {name: "James", numOfCall: 1},
+    {name: "Sams", numOfCall: 3,
+    {name: "Lalo", numOfCall: 2},
+];
+
+export function App(props) {
+    return console.log('hello')
+}
