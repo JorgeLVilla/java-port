31029a78111299faba0ac75650301f66994e6aba
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-11-23 23:38:13 -0800

    modern css reset

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
       <>
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>*/}
-        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        <img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+         Edit <code>src/App.js</code> and save to reload.
         {/*</p>*/}
         {/*<a*/}
         {/*  className="App-link"*/}
@@ -16,7 +16,7 @@ function App() {
         {/*  rel="noopener noreferrer"*/}
         {/*>*/}
         {/*  Learn React*/}
-        {/*</a>*/}
+        {/*</a>
           <button>Hellloooooo</button>
       </header>
     </div>
